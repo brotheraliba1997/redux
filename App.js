1d5eebf49500adac632d999ce484d17952adf844
hamza ali <61055100+brotheraliba1997@users.noreply.github.com>
2022-07-16 02:57:28 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Addtocart from './addtocart/Addtocart'
+import React from 'react';
+
+function App() {
+  return (
+    <div >
+      <Addtocart />
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ contant.js
@@ -0,0 +1,6 @@
+export const CART_ADD_ITEM = 'CART_ADD_ITEM';
+export const CART_ALL_REMOVE_ITEM = 'CART_ALL_REMOVE_ITEM';
+export const CART_DEC_ITEM_QTY = 'CART_DEC_ITEM_QTY';
+export const CART_INC_ITEM_QTY = 'CART_INC_ITEM_QTY';
+export const CART_REMOVE_ITEM = 'CART_REMOVE_ITEM';
+export const Cart_Add_Prices = 'Cart_Add_Prices';
